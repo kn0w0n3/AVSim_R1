f339d4f698351cae021fbf5995c4a3dc31c0230b
equati0n <tr14rc3@gmail.com>
2020-04-28 23:26:12 -0700

    Initial Commit
    City environment with object detection

---
+++ AVSim_R1/Assets/Follower.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Follower : MonoBehaviour {
+    public PathCreator pathCreator;
+    public float speed = 5;
+    float distanceTravelled;
+    float theDistance;
+    bool detected = false;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+
+        RaycastHit hit;
+       
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 20;
+        Debug.DrawRay(transform.position,forward,Color.green);
+
+        if (Physics.Raycast(transform.position,(forward),out hit)){
+            detected = true;
+            theDistance = hit.distance;
+            print(theDistance + " " + hit.collider.gameObject.name);
+            print("Detected object");
+            if (detected == true && theDistance < 15)
+            {
+                speed = 0;
+             
+            }
+
+        }
+        else
+        {
+            detected = false;
+            print("Object Not Detected");
+            if (speed < 15)
+            {
+                for (int i = 0; i != 60; i++)
+                {
+                    speed += 0.025f;
+                }         
+            }                          
+        }      
+    }
+}
---
+++ AVSim_R1/Assets/PedFollower.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class PedFollower : MonoBehaviour{
+    public PathCreator pathCreator;
+    public float speed = 1.5f;
+    float distanceTravelled;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        /*
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+        transform.Rotate(0, 0, 90);
+        */
+        transform.position += transform.forward * Time.deltaTime * speed;
+    }
+}
